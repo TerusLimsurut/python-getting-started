e5e5fd4e4e5da7c778b811f130291ea0d108d142
TerusLimsurut <few2012few@gmail.com>
2017-01-28 16:23:31 +0700

    .

--- app.py
+++ app.py
@@ -257,11 +257,7 @@ def handle_beacon(event):
 	
 if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-	
-    arg_parser = ArgumentParser(usage='Usage: python ' + __file__ + ' [--port <port>] [--help]')
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+    execute_from_command_line(sys.argv)
 
     # create tmp dir for download content
     make_static_tmp_dir()
