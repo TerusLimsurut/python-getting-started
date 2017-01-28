adc9467901fcf40fce31cd52e0560f15a635fdf5
TerusLimsurut <few2012few@gmail.com>
2017-01-28 16:21:22 +0700

    .

--- app.py
+++ app.py
@@ -254,14 +254,12 @@ def handle_beacon(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='Got beacon event. hwid=' + event.beacon.hwid))
-		
-
-
+	
 if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
+    execute_from_command_line(sys.argv)
+	
+    arg_parser = ArgumentParser(usage='Usage: python ' + __file__ + ' [--port <port>] [--help]')
     arg_parser.add_argument('-p', '--port', default=8000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
