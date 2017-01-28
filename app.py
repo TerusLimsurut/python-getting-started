7b306fc83ec7231eee30ae7cc1441c70a80769c3
TerusLimsurut <few2012few@gmail.com>
2017-01-28 16:26:44 +0700

    .

--- app.py
+++ app.py
@@ -259,13 +259,5 @@ if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-	
-    #arg_parser = ArgumentParser(usage='Usage: python ' + __file__ + ' [--port <port>] [--help]')
-    #arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    #arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    #options = arg_parser.parse_args()
-
-    make_static_tmp_dir()
-    app.run(debug=options.debug, port=options.port)
 
     
