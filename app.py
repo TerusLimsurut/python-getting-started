a8918ad2b8f38854532b58ba9ef14d767a8596a0
TerusLimsurut <few2012few@gmail.com>
2017-01-28 16:22:15 +0700

    .

--- app.py
+++ app.py
@@ -257,7 +257,6 @@ def handle_beacon(event):
 	
 if __name__ == "__main__":
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-    execute_from_command_line(sys.argv)
 	
     arg_parser = ArgumentParser(usage='Usage: python ' + __file__ + ' [--port <port>] [--help]')
     arg_parser.add_argument('-p', '--port', default=8000, help='port')
