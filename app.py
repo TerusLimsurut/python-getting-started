2aac9b9819755e7d895af471d9e3506563baf685
TerusLimsurut <few2012few@gmail.com>
2017-01-28 16:14:02 +0700

    .

--- app.py
+++ app.py
@@ -13,7 +13,7 @@
 #  License for the specific language governing permissions and limitations
 #  under the License.
 
-
+from django.core.management import execute_from_command_line
 
 from __future__ import unicode_literals
 
@@ -260,8 +260,6 @@ def handle_beacon(event):
 if __name__ == "__main__":
 	# FUse TEST #
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-
-    from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
 	#########
